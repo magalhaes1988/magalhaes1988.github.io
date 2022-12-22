84252041a4b69e5ba6139f5e442c683b4c0936f7
magalhaes1988 <magalhaes1988@gmail.com>
2022-12-21 22:33:32 -0300

    task-list-initial

--- src/modules/home/components/todo-list/todo-list.component.ts
+++ src/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { TaskList } from './../../../../modules/home/model/task-list';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+
+  public taskList: Array<TaskList>;
+
+  constructor() {
+
+    this.taskList = [];
+
+  }
+
+
+  ngOnInit(): void {
+
+  }
 
 }
